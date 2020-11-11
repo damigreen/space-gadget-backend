5691b92f3ad8c3a66cd3e1716d8e94f63e894172
Damilola Faseun <fashfired@gmail.com>
2020-11-11 08:46:35 +0100

    Created a scchema for customers

---
+++ customer.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const customerSchema = mongoose.Schema({
+  ID: String,
+  email: String,
+  first_name: String,
+  last_name: String,
+  active: Boolean,
+  inserted_at: Date,
+  updated_at: Date
+});
+
+
+customerSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
