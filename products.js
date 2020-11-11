fb85b7109c37d094e2caed8ee5faf1c06eb94688
Damilola Faseun <fashfired@gmail.com>
2020-11-11 08:46:46 +0100

    Created a scchema for products

---
+++ products.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  id: String,
+  SKU: String,
+  product_name: String,
+  product_description: String,
+  suplier_id: String,
+  category_id: String,
+  quantity_per_unit: Number,
+  unit_price: Number,
+  discount_price: Number,
+  // MSRP: Number,
+  //  availableSize: Number,
+   available_colors: { Type: [String] },
+  //  size: String,
+   color: String,
+   product_image: String,
+   inserted_at: Date,
+   updated_at: Date,
+});
+
+
+productSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
