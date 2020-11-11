b8a8e6fc693945a295bad3626b5373a5ee3b3446
Damilola Faseun <fashfired@gmail.com>
2020-11-11 21:39:13 +0100

    Added a mongoDB schema for categories

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+  category_id: String,
+  category_name: String,
+  category_desc: String,
+  category_image: String,
+  category_status: String
+});
+
+
+categorySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
