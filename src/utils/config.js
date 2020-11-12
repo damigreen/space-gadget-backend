267cd392f7a72464f4d9daf78bf90a29f9fdc2eb
Damilola Faseun <fashfired@gmail.com>
2020-11-12 21:14:55 +0100

    Created a config for environmental variables

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 let MONGO_URI = process.env.MONGO_URI;
 let PORT = process.env.PORT;
 
+
 // if (process.env.NODE_ENV === 'test') {
 //   MONGO_URI = process.env.TEST_MONGO_URI
 // }
