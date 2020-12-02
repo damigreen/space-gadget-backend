388de5fbe969a9377fc1f828806927f59ab8680a
Damilola Faseun <fashfired@gmail.com>
2020-12-02 22:35:29 +0100

    Added a route for soke products

---
+++ src/router/smokeProductRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const SmokeProduct = require('../models/smokeProduct');
+const smokeProductRouter = express.Router();
+
+smokeProductRouter.get('/', async (req, res) => {
+
+});
+
+smokeProductRouter.post('/', async (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  try {
+    const newSmokeProduct = await new SmokeProduct({
+      name: body.name,
+      brand: body.brand,
+      categories: body.description,
+      features: body.features
+    });
+
+    await newSmokeProduct.save();
+    res.send(newSmokeProduct.toJSON());
+
+  } catch (exception) {
+    console.log(exception);
+    next(exception);
+  }
+  
+});
+
+module.exports = smokeProductRouter;
