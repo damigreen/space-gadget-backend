da586f594912016d8682ed9c73b9fa41499e7bed
Damilola Faseun <fashfired@gmail.com>
2020-12-11 21:38:08 +0100

    Added fields for accessories

--- src/models/accessoriesProduct.js
+++ src/models/accessoriesProduct.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 var uniqueValidator = require('mongoose-unique-validator');
 
 const accessoriesProductSchema = new mongoose.Schema({
-  product_SKU: String,
-  product_name: String,
+  product_SKU: { type: String, required: true, unique: true },
+  product_name: { type: String, required: true, unique: true },
   product_model: String,
-  product_image: String,
+  product_image: [{ type: String }],
   product_color: String,
+  product_colors: [{ type: String }],
   product_description: [{ type: String }],
   product_features: [{ type: String }],
   product_specifications: [ { type: String }],
-  product_content: [ {type: String }]
+  product_content: [{ type: String }]
 });
 accessoriesProductSchema.plugin(uniqueValidator);
 
