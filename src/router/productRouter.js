da0baea3a5bd14d9915faec7e52100fe01c3e6a4
Damilola Faseun <fashfired@gmail.com>
2020-12-13 20:30:14 +0100

    Get and update a product

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -79,6 +79,83 @@ productRouter.post('/', async (req, res, next) => {
     console.log(`error ${e}`);
     next(e);
   }
+
 });
+
+productRouter.put('/:id', async (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  try {
+    const productObj = {
+
+      // product_id: uuidv4(),
+      product_SKU:  body.product_SKU,
+      product_name: body.product_name,
+      product_model: body.product_model,
+      product_description: body.product_description,
+      // supplier_id: "",
+      // category_id: "",
+      quantity_per_unit: body.product_per_unit,
+      unit_price: body.unit_price,
+      discount_price: body.discount_price,
+      available_colors: body.available_colors,
+      product_image: body.product_image,
+      inserted_at: new Date(),
+      updated_at: new Date(),
+      network: {
+        technology: body.network.technology
+      },
+      body: {
+        dimension: body.body.dimensions,
+        weight: body.body.weight
+      },
+      display: {
+        type: body.display.type,
+        size: body.display.size,
+        resolution: body.display.resolution
+      },
+      platform: {
+        os: body.platform.os,
+        chipset: body.platform.chipset,
+      },
+      memory: {
+        internal: body.memory.internal
+      },
+      camera: {
+        main: body.camera.main,
+        selfie: body.camera.selfie
+      },
+      battery: {
+        batteryType: body.battery.batteryType
+      },
+      misc: {
+        colors: body.misc.colors,
+        models: body.misc.models
+      }      
+    };
+
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, productObj, { new: true });
+    res.json(updatedProduct.toJSON())
+
+  } catch(e) {
+    console.log(e);
+  }
+
+});
+
+productRouter.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    console.log(product);
+    if (product) {
+      res.json(product.toJSON());
+    } else {
+      res.status(404).end();
+    }
+  } catch(e) {
+    console.log(e);
+  }
+})
 
 module.exports = productRouter;
